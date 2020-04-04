67042a27959fb6f6d25e3a8aa6825e0d624eec41
lSHWAR <63136881+lSHWAR@users.noreply.github.com>
2020-04-04 11:01:38 +0530

    Add files via upload

---
+++ Introduction/HelloNewWorld.java
@@ -0,0 +1,9 @@
+package Introduction;
+
+public class HelloNewWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello New World,Hows it goin");
+	}
+
+}
---
+++ operators/operators.java
@@ -0,0 +1,29 @@
+package operators;
+
+public class operators {
+
+	public static void main(String[] args) {
+		
+//		int a=100;
+//		int b=12;
+//		int c=b++; 
+//				System.out.println( c +" "+ b);
+//								
+//				int b = 6;
+//				int c = a<<3;
+//			System.out.println(c);
+//
+//				int a = 4;
+//				a|=2;
+//			    
+//				System.out.println(a);
+//			int a=7;
+//			int b=8;
+//				System.out.println(a!=b);
+		
+		
+		
+		
+	}
+
+}
